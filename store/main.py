78173f44aeae45c37b56bc68bc0a4c7945e75f65
Murilo Valenso <147419867+murilovalenso@users.noreply.github.com>
2024-05-15 17:52:18 -0300

    Add files via upload

--- store/main.py
+++ store/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+
 from store.core.config import settings
 from store.routers import api_router
+
 
 class App(FastAPI):
     def __init__(self, *args, **kwargs) -> None:
@@ -11,6 +13,7 @@ class App(FastAPI):
             title=settings.PROJECT_NAME,
             root_path=settings.ROOT_PATH
         )
+
 
 app = App()
 app.include_router(api_router)
